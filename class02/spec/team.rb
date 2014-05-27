9a702273f5d8a17f81594dd634926b9c535da613
aqt01 <lowell.abbott@gmail.com>
2014-05-27 12:55:35 -0400

    Copied template code from josh git

---
+++ class02/spec/team.rb
@@ -0,0 +1,15 @@
+class Team
+  def add_player(name, position)
+  end
+
+  def remove_player(name)
+  end
+
+  def filter_by_position(position)
+    # returns players in the given position
+  end
+
+  def to_s
+   # output team name + players list.
+  end
+end
