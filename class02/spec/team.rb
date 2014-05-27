5b53f8825a536ff9fb09f53140d667a1da4a51ab
aqt01 <lowell.abbott@gmail.com>
2014-05-27 13:02:53 -0400

    Add implementation tests classess in team

--- class02/spec/team.rb
+++ class02/spec/team.rb
@@ -1,15 +1,31 @@
 class Team
-  def add_player(name, position)
-  end
+	def add_player(name, position)
+	end
 
-  def remove_player(name)
-  end
+	def remove_player(name)
+	end
 
-  def filter_by_position(position)
-    # returns players in the given position
-  end
+	def filter_by_position(position)
+	    # returns players in the given position
+	end
 
-  def to_s
-   # output team name + players list.
-  end
+	def to_s
+	   # output team name + players list.
+	end
+
+	#Test
+	describe Team do
+		describe "#add_player" do
+		end
+		describe "#remove_player" do
+		end
+		describe "#filter_by_position(position)" do
+		end
+		describe "#to_s" do
+		end	
+	end
+	
+
 end
+
+
