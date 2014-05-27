9fa69df8823cb03864f4233d1be654e4454b2f57
aqt01 <lowell.abbott@gmail.com>
2014-05-27 14:17:20 -0400

    add test in new team

--- class02/spec/team.rb
+++ class02/spec/team.rb
@@ -37,6 +37,7 @@ class Team
 		le_output = "> "
 		@Players.each { |player| le_output += player.name + " Is " + player.position, " ; " }
 		le_output = le_output + "< "
+		return le_output
 	end
 
 	private
@@ -44,21 +45,38 @@ class Team
 	def team_size
 		return @Players.uniq.size
 	end
+	def team_empty?
+		team_size <= 0
+	end
+
 
 
 
 	#Test
 	describe Team do
+
 		describe "#new" do
+		     let(:team) { Team.new }
+
+                        it "should return a team instance" do
+                                expect (Team.new).to be_kind_of(Team)
+                        end
 		end
+
 		describe "#add_player" do
 		end
+
 		describe "#remove_player" do
 		end
+
 		describe "#filter_by_position" do
 		end
+
 		describe "#to_s" do
-		end	
+			if team_empty? 
+				:
+		end
+
 	end
 
 end
