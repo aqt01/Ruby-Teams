4c382fc2c5c5b7da0e47b104f28983f453ed0347
aqt01 <lowell.abbott@gmail.com>
2014-05-29 13:25:26 -0400

    implement product class and some methods in order class

--- class03/spec/class Order.rb
+++ class03/spec/class Order.rb
@@ -1,29 +1,37 @@
 require 'spec_helper'
+require 'yaml'
 
 class Order
 	attr_accessor :products
 
 	# Remember that working with YAML files YAML::load and YAML::dump are your friends!
 
-	def initialize
+	def initialize()
 	  	# loading or not loading should be the key here.
+        @products = Array.new
     end
  
-    def filter_by_category
+    def filter_by_category(category)
     	# Perhaps you could use the select method for arrays :)
+        @products.select { |product| product.category.casecmp(position).zero? }
     end
 
-    def filter_by_price
+    def filter_by_price(price)
     	# Perhaps the select method could work here too!
+        @products.select { |product| product.price.casecmp(price).zero? }
     end
  
-    def add_product
+    def add_product(name, price, category)
       # Remember stacks & queues? Pushing might be the answer.
+        new_product = Product.new(name,price,category)
+        @products.add(new_product)
+
     end
  
-    def get_product
+    def get_product(name)
     	# you query products by a product title but the catchy thing here is
     	# that if there are multiple matches only the first one is returned.
+        @products.select {|product| title == name }.first  
     end
  
     def save
--- class03/spec/product.rb
+++ class03/spec/product.rb
@@ -1,8 +1,19 @@
-class Product
-
+require 'spec_helper'
+require 'yaml'
 	# attribute names should be title, price, category
 	# where title is a String
 	# where price is a Float
 	# where category is a Symbol
+
+attr_accessor :name, :price, :category
+
+
+class Product
+
+	def initialize(name,price,category)
+		:name = name
+		:price = price
+		:category = category
+	end
 
 end
